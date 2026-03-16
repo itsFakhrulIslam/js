028cf50aa749ff69f09aec84ae6911d97946e93a
fimsHere <gmtelecom9089@gmail.com>
2026-03-17 01:49:11 +0600

    first commit in Array problemset 1 done

---
+++ array/problemSet1.js
@@ -0,0 +1,26 @@
+let numbers = [71, 72, 73, 74, 75, 76, 78, 79];
+// let sumN = numbers.length //total koto item
+// console.log(numbers);
+
+let fruitsBox = ["apple", "banana", "orange", "mango", "guava"];
+// let sumF = fruitsBox.length //total koto item
+// console.log(fruitsBox);
+
+let bazarBags = [
+  "alo",
+  "peyaj",
+  "begun",
+  "mula",
+  "gazor",
+  "tomato",
+  "deros",
+  "lao",
+  "shosha",
+  "potol",
+];
+// let sum = bazarBags.length
+// console.log(bazarBags);
+
+let oddNumbers = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+// let sumON = oddNumbers.length;
+// console.log(sumON);
