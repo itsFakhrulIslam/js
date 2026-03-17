516403e065ed881741c248623efddedb51d3185c
fimsHere <gmtelecom9089@gmail.com>
2026-03-18 00:56:01 +0600

    while loop done

---
+++ loops/while.js
@@ -0,0 +1,63 @@
+let num = 0;
+
+while (num <= 5) {
+  //   console.log(num);
+
+  num = num + 1;
+  //   num += 1;
+  //   num++;
+}
+
+//---------
+
+let say = 0;
+while (say < 5) {
+  // console.log("i love you pakhi!");
+
+  say++;
+}
+
+//--------------
+
+let consLogs = 0;
+while (consLogs < 10) {
+  //   console.log("i have practice 3hr/day programming!");
+
+  consLogs += 1;
+}
+
+// --------------
+
+let numb = 0;
+while (numb <= 20) {
+  //   console.log(numb);
+
+  numb++;
+}
+
+// -------------
+
+let numbe = 50;
+while (numbe < 100) {
+  //   console.log(numbe);
+
+  numbe++;
+}
+
+// ------------
+
+let n3num = 0;
+while (n3num < 10) {
+  //   console.log(n3num * 3);
+
+  n3num++;
+}
+
+// --------
+
+let numDiv = 110;
+while (numDiv < 111) {
+  //   console.log(numDiv/2);
+
+  numDiv++;
+}
