b863452b6ebf2ab1fd2745c907021258d4b55e3d
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-03-17 16:52:08 +0600

    problem set 2 done

---
+++ array/problemSet2.js
@@ -0,0 +1,23 @@
+let numSet = [1, 2, 3, 4, 5, 6, 7, 8, 9] //index[0,1, 2, 3, 4, 5, 6, 7, 8]
+let sum = numSet.length //item 9
+// console.log(sum);
+
+let familyMem = ['ma', 'baba', 'bon', 'vai']
+let indexNum = familyMem[0] //ma
+// console.log(indexNum);
+
+let undefineNum = [1, 2, 3, 4]
+let undefineItem = undefineNum[4] //undefined
+// console.log(undefineItem);
+
+let updateNum = [1, 2, 3, 5]
+updateNum[3] = 4 //value updated 5 to 4
+// console.log(updateNum);
+
+let friendsList = ['abu rayhan', 'tanjil', 'asad']
+let friendNum = friendsList[1]
+// console.log(friendNum);
+
+let updateFamilyMem = ['ma', 'baba', 'bon', 'vai']
+updateFamilyMem[1] = '' //father die 23.12.25
+// console.log(updateFamilyMem);
