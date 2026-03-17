00a72a32e0ca645771d565a70c6f9f548bf4530a
fimsHere <gmtelecom9089@gmail.com>
2026-03-18 01:25:47 +0600

    more example practice here

--- loops/while.js
+++ loops/while.js
@@ -61,3 +61,76 @@ while (numDiv < 111) {
 
   numDiv++;
 }
+
+// ---------
+let nums = 1;
+while (nums < 10) {
+  // console.log(nums);
+
+  nums++;
+}
+
+// ------------
+
+let number = 1;
+let sum = 0;
+while (number <= 10) {
+  // console.log(number);
+
+  sum = sum + number;
+  // console.log(sum);
+
+  number++;
+}
+// console.log(sum);
+
+// ==========
+
+let n = 50;
+while (n < 100) {
+  // console.log(n);
+
+  n++;
+}
+
+let sumN = 5;
+let sum1 = 0;
+while (sumN < 15) {
+  // console.log(sumN);
+
+  sum1 = sum1 + sumN;
+  // console.log(sum1);
+
+  sumN++;
+}
+
+let stuList = 1;
+let stuSum = 0;
+while (stuList < 50) {
+  // console.log(stuList);
+
+  stuSum += stuList;
+  // console.log(stuSum);
+
+  stuList++;
+}
+
+let sumNum = 21;
+let nSum = 0;
+while (sumNum < 50) {
+  // console.log(sumNum);
+
+  nSum += sumNum;
+  // console.log(nSum);
+
+  sumNum++;
+}
+
+let finalSum = 20;
+let fSum = 0;
+while (finalSum < 40) {
+  fSum += finalSum;
+
+  finalSum++;
+}
+// console.log(fSum);
