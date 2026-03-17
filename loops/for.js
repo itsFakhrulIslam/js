9e5e898f6503bab905c31322e9f8e3f50b1e6aaf
fimsHere <gmtelecom9089@gmail.com>
2026-03-18 02:04:13 +0600

    for loop is start here

---
+++ loops/for.js
@@ -0,0 +1,43 @@
+for (let num = 0; num < 10; num++) {
+  //   console.log(num);
+}
+for (let i = 0; i < 10; i = i + 1) {
+  //   console.log(i);
+}
+for (let i = 50; i < 100; i++) {
+  //   console.log(i);
+}
+
+// ----------
+
+let sum = 0;
+for (let i = 11; i < 20; i++) {
+  //   console.log(i);
+
+  sum = sum + i;
+  //   console.log(sum);
+}
+// console.log(sum);
+
+// ----------
+
+for (let i = 150; i < 170; i++) {
+  //   console.log(i);
+}
+
+// ------
+let stSum = 0;
+for (let i = 31; i < 58; i++) {
+  //   console.log(i);
+
+  stSum += i;
+}
+// console.log(stSum);
+
+// ---------
+
+let sums = 0;
+for (let i = 25; i < 75; i++) {
+  sums += i;
+}
+console.log(sums);
