024207c09a15feb9f7bd8f83aedffd0f2206bdfe
fimsHere <gmtelecom9089@gmail.com>
2026-03-19 01:10:31 +0600

    object start here ps1 done

---
+++ object/ps1.js
@@ -0,0 +1,74 @@
+const student = {
+  name: "kupa samsu",
+  age: 15,
+  class: 9,
+  isPolite: "so so",
+};
+// console.log(student);
+
+const phone = {
+  name: "16pro max",
+  brand: "apple",
+  price: 120000,
+  color: "silvar",
+};
+// console.log(phone);
+
+const juice = {
+  name: "green mango",
+  price: 150,
+  haveSugar: false,
+};
+// console.log(juice);
+
+const car = {
+  name: "BMW",
+  wheels: 4,
+  maxSpeed: 300,
+  headLight: true,
+};
+// console.log(car);
+
+//---------
+
+const subject = {
+  name: "bangla",
+  teacher: "hamid sir",
+  exam: {
+    date: "19.03.26",
+    marks: 100,
+  },
+  chapter: ["first", "thirds", "fifth"],
+  isFinalExam: true,
+};
+// console.log(subject);
+
+const teacher = {
+  name: "hanif sir",
+  teach: "english",
+  age: 34,
+  homeAddress: "cashara, narayanganj",
+};
+// console.log(teacher);
+
+const familyMember = {
+  father: {
+    name: "abdul kuddus gazi",
+    age: 63,
+    isAlive: false,
+  },
+  mother: {
+    name: "lutfa begum",
+    age: 50,
+    isAlive: true,
+  },
+  sonList: ["lucky", "jakia", "shamima", "miraj", "habiba"],
+  sonFamilyList: {
+    lucky: ["yousuf", "yeamin", "adiba"],
+    jakia: ["jahangir", "jikrullah", "jakaria"],
+    shamima: ["shahidul islam", "usman", "coming.."],
+    miraj: ["shahajadi", "coming..."],
+    habiba: ["rajib", "raiyan"],
+  },
+};
+// console.log(familyMember);
