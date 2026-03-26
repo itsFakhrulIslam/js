9a376a1187452daea5981ed3667c188257977ac3
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-03-26 20:19:43 +0600

    function start here with fps1 done

---
+++ function/fps1.js
@@ -0,0 +1,69 @@
+function namajeJa() {
+    console.log('light, fan off kor');
+    console.log('shutter nama');
+    console.log('tala laga');
+    console.log('mosjide ja');
+}
+// namajeJa()
+
+//! practice
+function juiceKhao() {
+    console.log('ai nao lemon juice!');
+}
+// juiceKhao()
+
+// ----
+
+function myRoutine() {
+    console.log('woke up early in the morning');
+    console.log('fresh to the face for prayer time');
+    console.log('take breakfast');
+    console.log('ready to go Shop!');
+}
+// myRoutine()
+
+// -----
+
+function familyMember() {
+    console.log('lutfa begum');
+    console.log('hosney ara');
+    console.log('jakia');
+    console.log('shamima');
+    console.log('miraj');
+    console.log('habiba');
+}
+// familyMember()
+
+// ----
+
+function taskDone() {
+    console.log('gom theke uth');
+    console.log('dokane ja');
+    console.log('dokan off kore namaje ja');
+    console.log('dokan off kore basay khaite ja');
+    console.log('dokan off kore ghumaite ja');
+}
+// taskDone()
+
+// ------
+
+function greetMe() {
+    console.log('morning! have a great day!');
+}
+// greetMe()
+
+// -----
+
+function closeFriend() {
+    console.log('tanjil');
+}
+// closeFriend()
+
+// -------
+
+function mySelf() {
+    console.log('miraj');
+    console.log('age: 25');
+    console.log('height: 5ft 3inc');
+}
+// mySelf()
