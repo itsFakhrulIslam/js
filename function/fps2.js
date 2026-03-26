519db18bf24b8fd5f6b1454ce69dc22f7e35719e
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-03-26 21:18:08 +0600

    fps2 some code added here

---
+++ function/fps2.js
@@ -0,0 +1,32 @@
+function average(pita, potro) {
+    let result = pita + potro / 2
+    console.log(result);
+}
+// average(20, 30)
+
+// ------
+
+function intoGon(a, b) {
+    let result = a * b
+    console.log(result);
+}
+// intoGon(20,15)
+
+// -----
+
+function subjectAverage(a, b, c) {
+    let result = a + b + c
+    let average = result / 3
+    console.log(average);
+}
+// subjectAverage(10, 20, 30)
+
+// ------
+
+function age(a) {
+    let older = a - 2004
+    console.log(older);
+}
+// age(2026)
+
+// --------
