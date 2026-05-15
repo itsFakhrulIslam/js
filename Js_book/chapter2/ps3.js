90c1bd7037cd1b13dfcf385f7511c10b44207824
fimsHere <gmtelecom9089@gmail.com>
2026-05-15 07:51:50 +0600

    chapter2 problem set

--- Js_book/chapter2/ps3.js
+++ Js_book/chapter2/ps3.js
@@ -6,3 +6,6 @@ console.log("my address is: ", address);
 
 let madrashaName = "narayanganj islamia kamil madrasha";
 console.log("my madrashaName is: ", madrashaName);
+
+let fan = "greatest is mohammad sm.";
+console.log("Im fan: ", fan);
