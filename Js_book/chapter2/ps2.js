6f6068cee20f12e5f9d68749518df7e09f43f7c9
fimsHere <gmtelecom9089@gmail.com>
2026-05-15 07:43:35 +0600

    chapter2 problem set

--- Js_book/chapter2/ps2.js
+++ Js_book/chapter2/ps2.js
@@ -4,3 +4,6 @@ console.log("myAge: ", myAge);
 let bookStudy = 2;
 console.log("bookStudy: ", bookStudy);
 
+let weakupSleepInTheMorning = "6am";
+console.log("weakupSleepInTheMorning everyday: ", weakupSleepInTheMorning);
+
