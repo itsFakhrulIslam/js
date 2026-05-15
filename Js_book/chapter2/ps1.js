b6e8c94f0441a2df4c729af05645647fcc225599
fimsHere <gmtelecom9089@gmail.com>
2026-05-15 07:33:41 +0600

    chapter2 problem set

--- Js_book/chapter2/ps1.js
+++ Js_book/chapter2/ps1.js
@@ -1,2 +1,5 @@
-let myBirthYear = 2004
-console.log('myBirthYear',myBirthYear);
+let myBirthYear = 2004;
+console.log("myBirthYear", myBirthYear);
+
+let siblings = 5;
+console.log("siblings", siblings);
