d7b349625828e432091623690a66201620713611
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-08-22 20:37:05 +0600

    data types learn done

--- js_foundation_program/module1/var_types.js
+++ js_foundation_program/module1/var_types.js
@@ -3,3 +3,15 @@ let age = 24;
 const birthDate = 2004;
 console.log(name, age, birthDate);
 console.log(typeof name, typeof age, typeof birthDate);
+
+// all data type in short name "ssbbnnuo"
+/**
+ * 1. string
+ * 2. symbol
+ * 3. boolean
+ * 4. big int
+ * 5. number
+ * 6. null
+ * 7. undefined
+ * 8. object
+ */
