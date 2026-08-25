883e8c02b5951a6b74d260ba4020aeb4fdd387a9
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-08-25 22:53:21 +0600

    all about for loops

---
+++ js_foundation_program/module2/loop.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 5; i++) {
+  console.log(`count: ${i}`);
+}
+
+const roller = ['miraj','siraj','riyaj','raj']
+for (let i = 0; i < roller.length; i++) {
+    const element = roller[i];
+    console.log(`${element}, yes sir present`);
+}
