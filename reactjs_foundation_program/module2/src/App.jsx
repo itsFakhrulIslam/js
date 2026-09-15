00c348f911fa579a8efa0d8c39ce23c4e3103c1d
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-09-15 12:21:07 +0600

    components understanding done

--- reactjs_foundation_program/module2/src/App.jsx
+++ reactjs_foundation_program/module2/src/App.jsx
@@ -1,10 +1,13 @@
-
+// import {TestComponents} from "./TestComponents"; //for named exports
+import TestComponents from "./TestComponents"; //for default exports
 
 function App() {
-
   return (
-    <h1>hello react world</h1>
-  )
+    <>
+      <h1>hello react world </h1>
+      <TestComponents />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ reactjs_foundation_program/module2/src/TestComponents.jsx
@@ -0,0 +1,30 @@
+//* its called export default / arrow function components
+// const TestComponents = () => {
+//   return (
+//     <div>
+//       <h1>Hello default TestComponents</h1>
+//     </div>
+//   );
+// };
+
+// export default TestComponents;
+
+//* its called named export
+// export const TestComponents = () => {
+//   return (
+//     <div>
+//       <h1>Hello named TestComponents</h1>
+//     </div>
+//   );
+// };
+
+//* its called normal functions components
+export default function TestComponents() {
+  return (
+    <div>
+      <h1>Hello normal func TestComponents</h1>
+      <p>me is a fakhrul islam</p>
+      <span>from bangladesh</span>
+    </div>
+  );
+}
