bf2a3a84d642c439d9058d5ac174b405e31c91ca
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-09-16 11:21:43 +0600

    something changed here

--- reactjs_foundation_program/module2/src/Card.jsx
+++ reactjs_foundation_program/module2/src/Card.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
+
 export default function Card({ name, quantity, rating }) {
-  const greetingsHandler = (getName, stock) => {
-    alert(`Hello ${getName}, available stock: ${stock}`);
+  const [getName, setGetName] = useState("");
+  console.log(getName);
+
+  const greetingsHandler = (getName) => {
+    setGetName(getName);
   };
 
   return (
     <>
       <div
-        onClick={() => greetingsHandler(name, quantity)}
+        onClick={() => greetingsHandler(name)}
         className=""
         style={{
           backgroundColor: "gray",
@@ -18,6 +23,8 @@ export default function Card({ name, quantity, rating }) {
         <h3>title: {name}</h3>
         <p>stock: {quantity}</p>
         <span>review: {rating}</span>
+        <br />
+        {getName && `welcome to ${getName}`}
       </div>
     </>
   );
